1ed823db2328fbfca994b011fffa45bfb48d8dc0
AGHOGHO UWEJEYAN <auwejeyan@gmail.com>
2019-02-19 22:30:34 +0100

    Initial commit

---
+++ src/App/Layout/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>Re-vent</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
